033a74c75cd4feb11ebf9d4e31e986dda84e3a16
Pooja Raut <pooja.raut264@gmail.com>
2021-11-13 15:48:02 -0600

    Part 1 & 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull(message="Location cannot be empty!")
+    @Size(max=50,message="Location is too long! Please enter up to 50 characters.")
+    private String location;
 
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotEmpty(message="Description cannot be blank!")
+    @Size(max=100,message="Description is too long! Please enter up to 100 characters.")
+    private String description;
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

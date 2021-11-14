5faf4bfc2298ec793e3edbe55b70f90593dbc7c8
Pooja Raut <pooja.raut264@gmail.com>
2021-11-14 02:10:29 -0600

    Part 3 & 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.models;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -8,6 +10,10 @@ public class Employer extends AbstractEntity {
     @NotNull(message="Location cannot be empty!")
     @Size(max=50,message="Location is too long! Please enter up to 50 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(){}
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @NotEmpty(message="Description cannot be blank!")
-    @Size(max=100,message="Description is too long! Please enter up to 100 characters.")
+    @Size(max=250,message="Description is too long! Please enter up to 250 characters.")
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill() {}
 
@@ -17,5 +22,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
